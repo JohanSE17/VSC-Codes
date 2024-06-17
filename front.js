fe4fd2922f3577b2dd407539c230be7e2030b101
JohanSE17 <johan.espitiap@campusucc.edu.co>
2024-06-17 16:46:07 -0500

    Variables

--- front.js
+++ front.js
@@ -1,1 +1,16 @@
 console.log("◘ Information VSC •")
+
+/* program js → en últimas versiones no es necesario el ; */
+
+let numa = 24
+let numb = 21
+
+let resp = numa + numb
+resp++
+
+console.log(resp)
+
+let name = "Joan"
+let well = "Saludo a " + name
+
+console.log(well)
